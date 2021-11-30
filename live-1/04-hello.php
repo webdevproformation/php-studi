a526dd22f980a07bb092acbd5b7ae179fa5a9e22
Malik H <malik.h@webdevpro.net>
2021-11-30 20:06:55 +0100

    fin live 1

---
+++ live-1/04-hello.php
@@ -0,0 +1,18 @@
+<?php echo "Bonjour tout le monde !" ; ?>
+<!-- http://localhost/php/live-1/04-hello.php -->
+
+<!-- echo => code répéter de ce qui écrit dans les "" --> 
+<?php echo "comment allez vous ?? "  ; ?>
+<!-- var_dump() --> 
+
+<?php var_dump("je vais bien merci !! ")  ; ?>
+<!-- permet afficher texte dans votre client => xdebug -->
+
+<!-- print_r() -->
+<?php print_r("fin du premier live !!  ")  ; ?>
+
+<?= "Bonjour 1 tout le monde !" ; ?>
+
+<!-- version raccourci -->
+
+<?= "Bonjour 2 tout le monde !" ; ?>
